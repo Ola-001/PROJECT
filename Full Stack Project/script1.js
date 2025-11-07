d92353d09cd48c71ae4849fc3160c895b630e53f
Ola-001 <olamidetadagbe@gmail.com>
2025-11-07 21:49:12 +0000

    creating a website about my photography.

---
+++ Full Stack Project/script1.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+alert("Welcome, this is my javascript file. c");
+alert("Let's get started with coding!");
+
+console.log("Hii I am a chat bot.");
+console.log("I am excited to learn full stack development.");
+console.log("Let's build something amazing together!");
+console.log("This is my first full stack project.");
+
+let botLocation = "Internet";
+console.log("I live on the " + botLocation + ".");
+
+let botName = "ChatBot";
+console.log("My name is " + botName + ".");
+
